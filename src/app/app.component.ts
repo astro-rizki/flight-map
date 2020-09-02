7611d60c63a13c6493e8830c358f4b1bde81e83b
astro-rizki <srizkiafrizal85@gmail.com>
2020-09-02 15:55:54 +0700

    add date/time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flight-map';
+
+  time: string;
+  date: string;
+  constructor() {
+    setInterval(() => {
+      this.time = new Date().toString().split(' ')[4];
+      this.date = new Date().toString().split(' ')[2] + " " +
+      new Date().toString().split(' ')[1] + " " +
+      new Date().toString().split(' ')[3];
+    }, 1);
+  }
 }
+
